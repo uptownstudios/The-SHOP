13dde62f46fa627bfc5b1103d26f984f289eebba
Brent Stromberg <brent@uptownstudios.net>
2016-05-17 17:59:16 -0700

    Edits to the home page

--- footer.php
+++ footer.php
@@ -83,7 +83,11 @@
 		});
 		$('.title-bar .menu-icon').click(function() {
 			$('body').toggleClass('off-canvas-open');
-		})
+		});
+		jQuery('.portfolio-filter-toggle a').click(function() {
+			$('#filters').slideToggle('fast');
+			return false;
+		});
 	});
 
 	function init() {
@@ -115,6 +119,49 @@
 	});
 
 </script>
+
+<script type="text/javascript">
+  var userFeed = new Instafeed({
+    get: 'user',
+    userId: 31411348,
+    accessToken: '609318530.467ede5.02e8a5cec1e54170a1eae5c20fb8c0dd',
+    limit: 24,
+    resolution: 'standard_resolution',
+    template: '<a class="instagram-image" href="{{link}}" target="_blank" title="{{caption}}"><img class="instafeed-img" src="{{image}}" /><span class="ig-caption">{{caption}}<br /><i class="fa fa-heart"></i> {{likes}} &nbsp;•&nbsp; <i class="fa fa-comment"></i> {{comments}}</span></a>',
+    after: function() {
+      jQuery('#instafeed').slick({
+        infinite: true,
+        slidesToShow: 6,
+        slidesToScroll: 6,
+        rows: 1,
+        centerPadding: '0px',
+        arrows: false,
+        autoplay: false,
+        autoplaySpeed: 5000,
+        easing: 'ease-out-back',
+        dots: true,
+        arrows: true,
+        prevArrow: '<button type="button" class="slick-prev"><i class="fa fa-chevron-left"></i></a>',
+        nextArrow: '<button type="button" class="slick-next"><i class="fa fa-chevron-right"></i></a>',
+        responsive: [{
+          breakpoint: 600,
+          settings: {
+            autoplay: false,
+          }
+        }],
+      });
+		(function($) {
+			$('a.instafeed-link').swipebox({
+				hideBarsDelay : 5000,
+				loopAtEnd: true,
+				removeBarsOnMobile: false
+			});
+		})(jQuery);
+      }
+  });
+  userFeed.run();
+</script>
+
 <?php do_action( 'foundationpress_before_closing_body' ); ?>
 <script id="__bs_script__">//<![CDATA[document.write("<script async src='http://HOST:3000/browser-sync/browser-sync-client.2.12.3.js'><\/script>".replace("HOST", location.hostname));
 //]]></script>
