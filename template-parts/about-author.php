e9deeadccccc3be2947159b35332bdb5f9f0635b
Brent Stromberg <brent@uptownstudios.net>
2016-07-21 17:36:55 -0700

    edits to the author by line and avatar placement on blog page

--- template-parts/about-author.php
+++ template-parts/about-author.php
@@ -3,7 +3,7 @@
 		<?php echo get_avatar( get_the_author_meta( 'ID' ), 80 ); ?>
 	</div>
 	<div class="author-description">
-		<strong><?php $authorName = the_author_meta('nickname'); echo $authorName; ?></strong><br>
+		<strong>By <?php $authorName = the_author_meta('nickname'); echo $authorName; ?></strong><br>
 		<?php $authorDesc = the_author_meta('description'); echo $authorDesc; ?>
 	</div>
 </div>
--- template-parts/content-blog.php
+++ template-parts/content-blog.php
@@ -9,7 +9,7 @@
  */
 
 $paged = ( get_query_var('paged') ) ? get_query_var('paged') : 1;
-$args = array( 'post_type' => 'post', 'posts_per_page' => 9, 'paged' => $paged );
+$args = array( 'post_type' => 'post', 'posts_per_page' => 18, 'status' => 'published', 'paged' => $paged );
 $loop = new WP_Query( $args );
 
 ?>
@@ -20,17 +20,21 @@ $loop = new WP_Query( $args );
 							$image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full' );
 							$image = $image[0];
 							$imageSrcSet = wp_get_attachment_image_srcset( get_post_thumbnail_id( $post->ID ), 'full' );
-							// $imageSrcSet = $imageSrcSet[0];
+							$imagealt = get_post_meta( get_post_thumbnail_id( $post->ID ), '_wp_attachment_image_alt', true );
+							if( empty($imagealt)) {
+								$imagealt = get_the_title() . ' Featured Image';
+							}
 						?>
 						<?php if ( has_post_thumbnail() ) { ?>
 						<div class="blog-page-featured-image">
-							<figure><a href="<?php the_permalink(); ?>"><img data-sizes="auto" data-src=<?php echo $image; ?>" data-srcset="<?php echo $imageSrcSet; ?>" data-expand="-110" class="lazyload" /><noscript><img src="<?php echo $image; ?>" /></noscript></a></figure>
+							<figure><a href="<?php the_permalink(); ?>"><img alt="<?php echo $imagealt; ?>" data-sizes="auto" data-src=<?php echo $image; ?>" data-srcset="<?php echo $imageSrcSet; ?>" class="lazyload" /><noscript><img src="<?php echo $image; ?>" alt="<?php echo $imagealt; ?>" /></noscript></a></figure>
 						</div>
+						<span class="avatar alt-avatar"><?php echo get_avatar( get_the_author_meta( 'ID' ), 50 ); ?></span>
 						<?php } ?>
 						<div class="blog-page-title-excerpt">
-							<h3><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h3>
+							<h3 <?php if ( has_post_thumbnail() ) { ?>class="alt-avatar-title"<?php } ?>><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h3>
 							<p class="author-date">Published on <?php the_date(); ?></p>
-							<p class="author-byline"><span class="avatar"><?php echo get_avatar( get_the_author_meta( 'ID' ), 32 ); ?></span><span class="byline">By <a href="<?php echo get_author_posts_url( $author_id ); ?>" title=""><?php the_author_meta( 'display_name', $author_id ); ?></a></span>
+							<p class="author-byline"><?php if ( !has_post_thumbnail() ) { ?><span class="avatar"><?php echo get_avatar( get_the_author_meta( 'ID' ), 50 ); ?></span><?php } ?><span class="byline">By <a href="<?php echo get_author_posts_url( $author_id ); ?>" title=""><?php the_author_meta( 'display_name', $author_id ); ?></a></span>
 							<?php the_excerpt(); ?></p>
 							<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">Read More &raquo;</a>
 						</div>
